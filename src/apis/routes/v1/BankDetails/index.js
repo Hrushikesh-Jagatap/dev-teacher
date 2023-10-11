c4b0cc02a54365f5cf5789a5f81e2d1f20876383
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-11 19:54:38 +0530

    Update index.js

--- src/apis/routes/v1/BankDetails/index.js
+++ src/apis/routes/v1/BankDetails/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+
 const router = express.Router();
-const UpdateEducationalDetailsByIdController = require('@controllers/v1/EducationalDetails')
-router.put('/updateEducationalDetails/:userId', async (req, res, next) => {
+
+const UpdateBankDetailsByIdController = require('@controllers/v1/BankDetails')
+
+router.put('/updateBankDetails/:userId', async (req, res, next) => {
     try {
-        const result = await UpdateEducationalDetailsByIdController.updateEducationalDetailsById(req, res, next);
+        const result = await UpdateBankDetailsByIdController.updateBankDetailsById(req, res, next);
     } catch (error) {
         console.error(error);
         next(error);
     }
+
 });
+
 module.exports = router;
