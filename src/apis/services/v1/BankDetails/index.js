8766d1b2a125e711811d9ddf76bff67aa417db6b
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-11 19:27:49 +0530

    Update index.js

--- src/apis/services/v1/BankDetails/index.js
+++ src/apis/services/v1/BankDetails/index.js
@@ -1,10 +1,22 @@
 const TeacherData = require('@models/Teacher');
+const _ = require('lodash');
+
+
 // Service function to update a BankDetails by UserID
 const updateBankDetailsById = async (userId, updateBankDetails) => {
   try {
+
+    const user = await TeacherData.findOne({ userId: userId });
+
+    if (!user) {
+      return "User not found In Db"
+    }
+
+    const mergedBankDetails = _.merge({}, user.bankDetails, updateBankDetails);
+
     const updatedTeacher = await TeacherData.findOneAndUpdate(
       { userId },
-      { $set: { bankDetails: updateBankDetails } },
+      { $set: { bankDetails: mergedBankDetails } },
       { new: true }
     );
     return updatedTeacher;
@@ -12,6 +24,7 @@ const updateBankDetailsById = async (userId, updateBankDetails) => {
     throw new Error('Failed to update BankDetails');
   }
 };
+
 module.exports = {
   updateBankDetailsById,
 };
