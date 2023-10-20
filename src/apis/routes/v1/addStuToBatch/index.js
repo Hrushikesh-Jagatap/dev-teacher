9f180b96cbf7b52f772dd87f9d563dc9db19d918
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-20 13:00:21 +0530

    Update index.js

--- src/apis/routes/v1/addStuToBatch/index.js
+++ src/apis/routes/v1/addStuToBatch/index.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const addBatchToStuController = require('@controllers/v1/addStuToBatch');
-router.put('/add-to-batch/:userId', async (req, res, next) => {
+onst Batch = require('@models/Batch');
+const TeacherData = require('@models/Teacher');
+// add  Student To Batch Service
+const addToBatch = async (userId, data) => {
     try {
-        const result = await addBatchToStuController.addToBatch(req, res, next);
+        const studentIdToAdd = data.student_userId
+        const user = await TeacherData.findOne({ userId: userId });
+        if (!user) {
+            return ('Teacher Does Not Exist');
+        }
+        const { batch_id } = data;
+        const batch = await Batch.findOne({ batch_id: batch_id });
+        if (!batch) {
+            return ('Batch not found');
+        }
+        if (batch.student_userId.includes(studentIdToAdd)) {
+            return ('Invalid student ID or student already in the batch');
+        }
+        // Add the student to the batch
+        batch.student_userId.push(...studentIdToAdd);
+        await batch.save();
+        return { studentIdToAdd };
     } catch (error) {
-        next(error);
+        console.error(error);
+        res.status(500).json({ error: 'Failed to add student to the batch' });
     }
-});
-module.exports = router;
+};
+module.exports = {
+    addToBatch,
+};
