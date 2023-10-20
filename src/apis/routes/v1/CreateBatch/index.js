c691d9706403c72bd066f898b489c2e1e90dcdad
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-20 12:51:31 +0530

    Update index.js

--- src/apis/routes/v1/CreateBatch/index.js
+++ src/apis/routes/v1/CreateBatch/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const CreateBatchController = require('@controllers/v1/CreateBatch');
 
-router.post('/batch', async (req, res, next) => {
+router.post('/create-batch', async (req, res, next) => {
     // Extract the necessary data from the request body
     try {
         const result = await CreateBatchController.createBatches(req, res, next);
