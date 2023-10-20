fb96d0f89efd5fbc3e37a5d0d9803618a19b2483
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-20 12:52:06 +0530

    Create index.js

---
+++ src/apis/routes/v1/CreateChapter/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const CreateChapterController = require('@controllers/v1/CreateChapter');
+router.post('/create-chapter', async (req, res, next) => {
+    // Extract the necessary data from the request body
+    try {
+        const result = await CreateChapterController.createChapter(req, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+module.exports = router;
