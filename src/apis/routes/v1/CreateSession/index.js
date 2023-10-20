e30e3c798af84bc25f6c322cb78610b2ce97aa8f
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-20 12:52:33 +0530

    Create index.js

---
+++ src/apis/routes/v1/CreateSession/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const CreateSessionController = require('@controllers/v1/CreateSession');
+router.post('/create-session', async (req, res, next) => {
+    // Extract the necessary data from the request body
+    try {
+        const result = await CreateSessionController.createSession(req, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+module.exports = router;
