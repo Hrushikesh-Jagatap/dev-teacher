70d0491593e0c81c0b7c6227dcbf61a81c8af71a
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-20 12:47:25 +0530

    Update index.js

--- src/apis/models/index.js
+++ src/apis/models/index.js
@@ -4,6 +4,9 @@ const Address = require('./Address');
 const Favourite = require('./Favourite');
 const Organization = require('./Organization');
 const Batch = require('./Batch');
+const Chapter = require('./Chapter');
+const Session = require('./Session');
+
 const AppVersion = require('./AppVersion');
 module.exports = {
   Employee,
@@ -13,4 +16,6 @@ module.exports = {
   Organization,
   Batch,
    AppVersion,
+  Chapter,
+  Session,
 };
