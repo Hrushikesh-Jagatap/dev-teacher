d67ae7e4e4836602741832dcdceeca581d515bd6
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-23 14:23:31 +0530

    Update index.js

--- src/apis/controllers/v1/updateSession/index.js
+++ src/apis/controllers/v1/updateSession/index.js
@@ -2,11 +2,10 @@ const sessionService = require('@services/v1/updateSession')
 const { HttpResponseHandler } = require('intelli-utility');
 const chapterDb = require('@models/Chapter');
 const updateSession = async (req, res, next) => {
- 
-  try {
-    const sessionId = req.params.Id;  
-    // Get the session ID from the URL
-    const updatedSessionData = req.body;      // The updated session data from the request body
+   try {
+    const sessionId = req.params.Id;   // Get the session ID from the URL
+        const updatedSessionData = req.body;  // The updated session data from the request body
+
     const updateSession = await sessionService.updateSession(sessionId, updatedSessionData);
  
     if (!updateSession) {
