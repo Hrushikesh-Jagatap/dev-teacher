fcdfecf2732d8ce4d25f14d1253d71f3ce2a626d
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-24 20:13:05 +0530

    Update index.js

--- src/apis/services/v1/getBatchByTeacherId/index.js
+++ src/apis/services/v1/getBatchByTeacherId/index.js
@@ -3,7 +3,7 @@ const BatchData = require('@models/Batch');
 // Service function to get a single batch by teacher ID
 const getBatchByTeacherId = async (userId) => {
   try {
-    const batch = await BatchData.findOne({ userId: userId });
+    const batch = await BatchData.find({ userId: userId });
     return batch;
   } catch (error) {
     throw new Error('Failed to get Batch By TeacherId');
