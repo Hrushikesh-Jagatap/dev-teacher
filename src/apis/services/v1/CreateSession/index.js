27600afb4d7b24b2b5ee9bd37a424fd3285b83a2
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-24 13:14:06 +0530

    Update index.js

--- src/apis/services/v1/CreateSession/index.js
+++ src/apis/services/v1/CreateSession/index.js
@@ -1,27 +1,33 @@
 const sessionDb = require('@models/Session');
 const { createUUID } = require('@common/libs/UUID/UUIDV4');
 const { addSessionToChapter } = require('@services/v1/updateChapter')
+
+
 // Service function to create a new session in  chapter
 const createSession = async (sessionData) => {
     try {
+
         const session_id = `SESSION_${createUUID()}`;
+
         sessionData.session_id = session_id;
-       
+
         const { chapter_id } = sessionData;
-         const updateChapter = await addSessionToChapter(chapter_id, session_id)
+
+        const updateChapter = await addSessionToChapter(chapter_id, session_id)
+
         if (updateChapter === 'chapter not found') {
             return ('Error while updating sessionId in chapter');
         }
-        const newSession = await sessionDb.crechapter_idate(sessionData);
-        // await chapterDb.findOneAndUpdate(
-        //     { chapter_id },
-        //     { $push: { session: { Id: session_id } } }
-        // );
+
+        const newSession = await sessionDb.create(sessionData);
+        
         return newSession;
+
     } catch (error) {
         throw new Error('Failed to create new Session for chapter');
     }
 };
+
 module.exports = {
     createSession
 };
