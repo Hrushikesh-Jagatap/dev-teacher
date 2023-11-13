67736d380fc4e78ce5393b04a1ca9b6ec3e81fbd
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-13 13:10:35 +0530

    Update index.js

--- src/apis/routes/v1/searchforinstance/index.js
+++ src/apis/routes/v1/searchforinstance/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
+
 const searchforinstanceController = require('@controllers/v1/searchforinstance');
+
 router.get('/searchforinstance', async (req, res, next) => {
     try {
         const result = await searchforinstanceController.searchTeacher(req, res, next);
@@ -8,4 +11,6 @@ router.get('/searchforinstance', async (req, res, next) => {
         next(error)
     }
 });
+
 module.exports = router;
+
