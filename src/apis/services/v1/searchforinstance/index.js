e375a1626f6e36b7b8e32ad9127866d280535e38
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-13 13:11:35 +0530

    Update index.js

--- src/apis/services/v1/searchforinstance/index.js
+++ src/apis/services/v1/searchforinstance/index.js
@@ -1,21 +1,27 @@
 // const TeacherData = require('@models/Teacher');
+
 // const searchTeacher = async (query) => {
 //   try {
 //     const {  subject, className, lang } = query;
 // const instance_status1 ="Active"
 //     const filter = {};
+
 // //   if (mode && Array.isArray(mode) && mode.length > 0) {
 // //   filter["teachingDetails.teaching_mode"] = mode;
 // // }
+
 //     if (subject) {
 //       filter["teachingDetails.subjects_taught.subject"] = new RegExp(subject, 'i');
 //     }
+
 //     if (className) {
 //       filter["teachingDetails.subjects_taught.class"] = new RegExp(className, 'i');
 //     }
+
 //     if (lang) {
 //       filter["educationDetails.teaching_languages"] = new RegExp(lang, 'i');
 //     }
+
 //     if(instance_status1){
 //             filter["instance_status"] = new RegExp(instance_status1);
 //     }
@@ -23,6 +29,7 @@
 //     const results = await TeacherData.find(filter)
 //       .select("userId  personalDetails teachingDetails educationDetails OnlieTeachingDeatis OfflineTeachingDeatis instance_status")
 //       .lean().exec();
+
 //       if (results === null) {
 //         return {
 //           status: 404,
@@ -35,45 +42,63 @@
 //     throw new Error('Failed to find teachers');
 //   }
 // };
+
 // module.exports = {
 //   searchTeacher,
 // };
 const Teacher = require('@models/Teacher');
+
 const searchTeacher = async (query) => {
   try {
-    const { subject, className, lang} = query;
+    const { subject, className, lang,studentid} = query;
     let status=true;
-    const filter = {};
+    // const filter = {'student_userId.student_userId': studentid,'student_userId.subject':subject,'student_userId.class':className,};
+const filter={};
     if (subject) {
-      filter["teachingDetails.subjects_taught.subject"] = new RegExp(subject, 'i');
+      filter["OnlieTeachingDeatis.subjects_taught.subject"] = new RegExp(subject, 'i');
     }
+    //  if (subject) {
+    //   filter["teachingDetails.subjects_taught.subject"] = new RegExp(subject, 'i');
+    // }
+
  if (status) {
       filter["instance_status.status"] =status
     }
+
+
+
     if (className) {
-      filter["teachingDetails.subjects_taught.class"] = new RegExp(className, 'i');
+      filter["OnlieTeachingDeatis.subjects_taught.class"] = new RegExp(className, 'i');
     }
+
     if (lang) {
-      filter["educationDetails.teaching_languages"] = new RegExp(lang, 'i');
+      filter["OnlieTeachingDeatis.teaching_languages"] = new RegExp(lang, 'i');
     }
+
+     
+
    
     console.log(filter);
+
     const results = await Teacher.find(filter)
      // .select("userId personalDetails teachingDetails educationDetails OnlineTeachingDetails OfflineTeachingDetails instance_status")
-      .select() 
+      .select("userId") 
      .lean()
       .exec();
+
     if (results.length === 0) {
       return {
         status: 404,
         message: 'NO_TEACHER_FOUND',
       };
     }
+
     return results;
   } catch (error) {
     throw new Error('Failed to find teachers');
   }
 };
+
 module.exports = {
   searchTeacher,
 };
